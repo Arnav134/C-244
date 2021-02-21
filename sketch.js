dfc21578c9778f6e9545f936250b91f419e3dedc
Arnav134 <76956100+Arnav134@users.noreply.github.com>
2021-02-21 21:28:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+
+	
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	boxleftSprite=createSprite(boxPosition,boxY,20,100);
+	boxleftSprite.shapeColor=color("brown");
+
+	boxLeftBody = Bodies.rectangle(boxPosition+20,boxY,20,100, {isStatic:true} );
+	World.add(world,boxLeftBody);
+
+	boxBase=createSprite(boxPosition+100,boxY+40,200,20);
+	boxBase.shapeColor=color(255,0,0);
+
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+
+}
